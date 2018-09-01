eadcc7dfac0a330275fe42758025363b3357aeab
Shawn Lower <shawn@shawnlower.com>
2018-09-01 18:21:45 -0400

    Add initial code w/ arg parsing

---
+++ go-ltp/cmd/add.go
@@ -0,0 +1,72 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+    "os"
+
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add an item to the repository",
+    Long: `
+Add an item to the repository:
+
+New items can be created from multiple sources, such as:
+  - Local files and directories
+  - Dereferenceable URLs
+  - Standard input (pipes, etc)
+
+Examples:
+    # add from standard-input
+    xclip -o | go-ltp add -
+
+    # add from a local file
+    go-ltp add ~/Pictures/Jamaica_Bay.png
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+        // Attempt to parse each of the inputs provided
+        for _, inputString := range args {
+            switch inputString {
+            case "-":
+                fmt.Println("Reading from stdin...")
+
+            default:
+                // Unknown input type
+                fmt.Println("Invalid: " + inputString)
+                os.Exit(1)
+            }
+
+        }
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
